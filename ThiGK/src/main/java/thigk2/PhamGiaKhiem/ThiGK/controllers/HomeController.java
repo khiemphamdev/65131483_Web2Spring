3e392acccc24fa10389b723d1add754cfcfb4f99
khiemphamdev <khiempham.dev@gmail.com>
2026-04-24 14:40:41 +0700

    nop

--- ThiGK/src/main/java/thigk2/PhamGiaKhiem/ThiGK/controllers/HomeController.java
+++ ThiGK/src/main/java/thigk2/PhamGiaKhiem/ThiGK/controllers/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import thigk2.PhamGiaKhiem.ThiGK.models.Product;
+import thigk2.PhamGiaKhiem.ThiGK.services.CategoryService;
 import thigk2.PhamGiaKhiem.ThiGK.services.ProductService;
 
 
@@ -19,12 +20,22 @@ public class HomeController {
 
     @Autowired
     private ProductService productService;
-
+    @Autowired
+    private CategoryService categoryService;
     // 1. Hiển thị danh sách tất cả sản phẩm (Trang người dùng)
-    @GetMapping("")
-    public String listProducts(Model model) {
-        model.addAttribute("dsSanPham", productService.getAll());
-        return "product_list"; // Tên file HTML: product_list.html
+    @GetMapping
+    public String listAllProducts(Model model) {
+        model.addAttribute("products", productService.getAll());
+        model.addAttribute("categories", categoryService.getAll());
+        return "index"; // Trả về file product-list.html trong thư mục templates
+    }
+ // Lấy sản phẩm theo loại
+    @GetMapping("/category/{id}")
+    public String listProductsByCategory(@PathVariable("id") Integer categoryId, Model model) {
+        model.addAttribute("products", productService.getProductsByCategoryId(categoryId));
+        model.addAttribute("categories", categoryService.getAll());
+        model.addAttribute("selectedCatId", categoryId); // Để đánh dấu loại đang chọn
+        return "index"; 
     }
 
     // 2. Xem chi tiết một sản phẩm
@@ -34,7 +45,7 @@ public class HomeController {
         if (p == null) {
             return "redirect:/products"; // Trả về danh sách nếu không thấy sản phẩm
         }
-        model.addAttribute("sp", p);
+        model.addAttribute("products", p);
         return "product_detail";
     }
 }
